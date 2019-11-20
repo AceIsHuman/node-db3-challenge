db84fdfeb0c8529254bf3505f33d1bc14b1e2ba5
AceIsHuman <menoman1995@yahoo.com>
2019-11-20 01:47:44 -0600

    structure model functions and export

---
+++ schemes/scheme-model.js
@@ -0,0 +1,24 @@
+const db = require('../knexfile.js');
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove
+}
+
+function find() {
+}
+
+function findById(id) {
+}
+
+function add(user) {
+}
+
+function update(changes, id) {
+}
+
+function remove(id) {
+}
